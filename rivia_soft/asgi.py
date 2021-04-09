8457edbd96c5e009ef874ac6c18302931467cc89
Iftakhar Husan <iftakharn19@gmail.com>
2021-04-09 17:11:52 +0600

    update project name from RivaSoft to rivia_soft

--- rivia_soft/asgi.py
+++ rivia_soft/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for RiviaSoft project.
+ASGI config for rivia_soft project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RiviaSoft.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rivia_soft.settings')
 
 application = get_asgi_application()
--- rivia_soft/wsgi.py
+++ rivia_soft/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for RiviaSoft project.
+WSGI config for rivia_soft project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RiviaSoft.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rivia_soft.settings')
 
 application = get_wsgi_application()

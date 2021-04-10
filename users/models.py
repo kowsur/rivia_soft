981de6d295fdab03ee1fc1564109da76fd870a33
Iftakhar Husan <iftakharn19@gmail.com>
2021-04-11 01:19:23 +0600

    add CustomUser model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, AbstractUser
+from django.contrib.auth.models import PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
 
-# Create your models here.
+from .managers import CustomUserManager
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+
+    class Meta:
+        verbose_name = _("User")
+        verbose_name_plural = _("Users")
+
+    user_id = models.BigAutoField(verbose_name='user id', unique=True, editable=False, primary_key=True, db_index=True)
+    email = models.EmailField(_('email address'), unique=True, db_index=True)
+    first_name = models.CharField(max_length=64, blank=True, db_index=False)
+    last_name = models.CharField(max_length=64, blank=True, db_index=False)
+
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        if self.get_full_name():
+            return self.get_full_name()
+        elif self.first_name:
+            return self.first_name
+        elif self.last_name:
+            return self.last_name
+        else:
+            return self.email
+    
+    def get_username(self) -> str:
+        return self.get_email()
+    
+    def get_email(self) -> str:
+        return self.email
+    
+    def get_short_name(self) -> str:
+        if self.first_name:
+            return self.first_name
+        if self.last_name:
+            return self.last_name
+        return str(self._meta.verbose_name)
+    
+    def get_full_name(self) -> str:
+        if self.first_name and self.last_name:
+            return f'{self.first_name} {self.last_name}'
+        return self.get_short_name()

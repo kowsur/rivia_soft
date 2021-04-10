e8374866f9c4e8d517cf591c8c42957dd879e72c
Iftakhar Husan <iftakharn19@gmail.com>
2021-04-11 01:19:52 +0600

    add forms

---
+++ users/forms.py
@@ -0,0 +1,39 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django import forms
+
+from .models import CustomUser
+
+
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'first_name','last_name', 'password', 'is_superuser', 'is_staff', 'date_joined')
+
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'first_name', 'last_name', 'password', 'is_superuser', 'is_staff', 'date_joined')
+
+
+class CustomUserSignupForm(UserCreationForm):
+
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'first_name', 'last_name', 'password1', 'password2')
+
+
+class CustomUserLoginForm(forms.Form): # Note: forms.Form NOT forms.ModelForm
+    
+    email = forms.EmailField(widget=forms.TextInput(
+        attrs={'class': 'form-control','type':'text','name': 'email','placeholder':'Email'}), 
+        label='Email')
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={'class':'form-control','type':'password', 'name': 'password','placeholder':'Password'}),
+        label='Password')
+
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'password']

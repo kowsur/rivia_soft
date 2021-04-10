b18394b7ac48ca49425e2258e6dd3f995cb120bf
Iftakhar Husan <iftakharn19@gmail.com>
2021-04-11 01:21:19 +0600

    register CustomUser model to admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ('email', 'is_active', 'is_staff', 'is_superuser')
+    list_filter = ('is_superuser', 'is_staff', 'is_active')
+    fieldsets = (
+        ('User Info', {
+                        'classes': ('wide',),
+                        'fields': ('email', 'password', 'first_name', 'last_name', 'date_joined')
+                      }
+        ),
+        ('Permissions', {
+                        'classes': ('wide',),
+                        'fields': ('is_superuser', 'is_staff', 'is_active')
+                        }
+        ),
+    )
+    add_fieldsets = (
+        ('User Info', {
+                        'classes': ('wide',),
+                        'fields': ('email', 'password1', 'password2', 'first_name', 'last_name', 'date_joined')
+                      }
+        ),
+        ('Permissions', {
+                        'classes': ('wide',),
+                        'fields': ('is_superuser', 'is_staff', 'is_active')
+                        }
+        )
+    )
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email','first_name', 'last_name')
+
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.unregister(Group)

3639310eb92e5ef0161efd0aafb496903c2b825e
Iftakhar Husan <iftakharn19@gmail.com>
2021-04-11 19:18:51 +0600

    add search functions for users

--- users/queries.py
+++ users/queries.py
@@ -1,11 +1,27 @@
 from .models import CustomUser
 from django.db.models import Q
 
-def CustomUser_search_by_email(search_text:str, limit:int = -1):
+def search_CustomUser_by_email(search_text:str, limit:int = -1):
     if limit<-1:
         raise ValueError("Limit can't be smaller than -1")
+    
     Query = Q(email__contains = search_text)
-    results = CustomUser.objects.filter(Query).only(fields=('email'))
+
+    results = CustomUser.objects.filter(Query).only('user_id')
+    if limit==-1:
+        return results
+    return results[:limit]
+
+
+def search_CustomUser(search_text:str, limit:int = -1):
+    if limit<-1:
+        raise ValueError("Limit can't be smaller than -1")
+    
+    Query = Q(email__contains       = search_text)|\
+            Q(first_name__contains  = search_text)|\
+            Q(last_name__contains   = search_text)
+    
+    results = CustomUser.objects.filter(Query).only('user_id')
     if limit==-1:
         return results
     return results[:limit]

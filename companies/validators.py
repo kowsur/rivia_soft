db6328593dd60f7f58647e273ed6633d7a623ec7
Iftakhar Husan <iftakharn19@gmail.com>
2021-04-11 16:32:29 +0600

    make error messages helpful

--- companies/validators.py
+++ companies/validators.py
@@ -17,7 +17,7 @@ NINO_REGEX = r'^(?!BG|GB|KN|NK|NT|TN|ZZ)[ABCEGHJKLMNOPRSTWXYZ][ABCEGHJKLMNPRSTWX
 COMPILED_NINO_REGEX = re.compile(NINO_REGEX, flags=re.IGNORECASE)
 NINO_VALIDATOR = RegexValidator(
     regex=COMPILED_NINO_REGEX,
-    message='Enter a valid National Insurance Number.',
+    message='Enter 9 chars long NINO.',
     code='Invalid NINO')
 
 # Sort Code
@@ -25,7 +25,7 @@ SORT_CODE_REGEX = r'^[0-9]{6}$'
 COMPILED_SORT_CODE_REGEX = re.compile(SORT_CODE_REGEX)
 SORT_CODE_VALIDATOR = RegexValidator(
     regex=COMPILED_SORT_CODE_REGEX,
-    message='Enter a valid Sort Code',
+    message='Enter 6 digit Sort Code',
     code='Invalid Sort Code')
 
 # Bank Account Number
@@ -33,6 +33,6 @@ BANK_ACCOUNT_NUMBER_REGEX = r'^[0-9]{8}$'
 COMPILED_BANK_ACCOUNT_NUMBER = re.compile(BANK_ACCOUNT_NUMBER_REGEX)
 BANK_ACCOUNT_NUMBER_VALIDATOR = RegexValidator(
     regex=COMPILED_BANK_ACCOUNT_NUMBER,
-    message='Enter a Valid Bank Account Number',
+    message='Enter 8 digit Bank Account Number',
     code='Invalid Bank Account'
 )

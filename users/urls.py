f65b9621147a1d6c546342906e663c001356d4ee
Iftakhar Husan <iftakharn19@gmail.com>
2021-04-16 10:36:21 +0600

    update url name

--- users/urls.py
+++ users/urls.py
@@ -5,11 +5,9 @@ from .views import login_user, search_users_by_email, search_users_extended, sig
 
 urlpatterns = [
     path('', login_user, name='users'),
-    # path('signup/', signup_user, name='users-signup'),
+    # path('signup/', signup_user, name='users_signup'),
     path('login/', login_user, name='users_login'),
     path('logout/', logout_user, name='users_logout'),
     path('search/<str:search_text>/', search_users_extended, name='search_users'),
     # path('search_email/<str:search_text>/', search_users_by_email, name='search_users_by_email'),
-
-    re_path('.*', login_user, name='users'), #matches anything
 ]

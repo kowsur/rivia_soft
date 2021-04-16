e8532883d8fa16dbd3b438aa6e0c964d5dde3a39
Iftakhar Husan <iftakharn19@gmail.com>
2021-04-16 10:39:42 +0600

    include companies urls

--- rivia_soft/urls.py
+++ rivia_soft/urls.py
@@ -14,14 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.http import request
-from django.http.response import HttpResponse
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('', lambda request: HttpResponse('<h1>home</h1>'), name='home'),
+    path('', include('companies.urls'), name='home'),
     path('u/', include('users.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

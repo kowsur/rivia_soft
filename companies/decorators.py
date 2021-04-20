84c76f5f0715de673ae88ba8089b1c91d349aee3
Iftakhar Husan <iftakharn19@gmail.com>
2021-04-20 11:54:05 +0600

    allow to add message and redirect in permission checkers

--- companies/decorators.py
+++ companies/decorators.py
@@ -1,18 +1,24 @@
+from django.contrib import messages
 from django.shortcuts import redirect
 
+def allowed_for_staff(message="Sorry! You are not authorized to see this.", redirect_to='home'):
+    def outer_decorator(view_func):
+        def wrapper(request, *args, **kwargs):
+            if request.user.is_staff:
+                return view_func(request, *args, **kwargs)
+            else:
+                messages.error(request, message)
+                return redirect(redirect_to)
+        return wrapper
+    return outer_decorator
 
-def allowed_for_staff(view_func):
-    def wrapper(request, *args, **kwargs):
-        if request.user.is_staff:
-            return view_func(request, *args, **kwargs)
-        else:
-            return redirect('companies-home')
-    return wrapper
-
-def allowed_for_superuser(view_func):
-    def wrapper(request, *args, **kwargs):
-        if request.user.is_superuser:
-            return view_func(request, *args, **kwargs)
-        else:
-            return redirect('companies-home')
-    return wrapper
+def allowed_for_superuser(message="Sorry! You are not authorized to see this.", redirect_to='home'):
+    def outer_decorator(view_func):
+        def wrapper(request, *args, **kwargs):
+            if request.user.is_superuser:
+                return view_func(request, *args, **kwargs)
+            else:
+                messages.error(request, message)
+                return redirect(redirect_to)
+        return wrapper
+    return outer_decorator

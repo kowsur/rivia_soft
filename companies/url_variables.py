511bd14d72ff601d4d7eec9521789792978674cc
Iftakhar Husan <iftakharn19@gmail.com>
2021-04-27 00:33:53 +0600

    change url paths and names to use variables from url_variables.py

--- companies/url_variables.py
+++ companies/url_variables.py
@@ -1,3 +1,5 @@
+APPLICATION_NAME = 'companies'
+
 # url paths 'companies/model_path/action/'
 home_suffix = 'home'
 create_suffix = 'create'
@@ -6,8 +8,6 @@ delete_suffix = 'delete'
 search_suffix = 'search'
 viewall_suffix = 'all'
 
-application_name = 'companies'
-
 
 Selfassesment_path = 'SA'
 Selfassesment_name = 'SA'
@@ -15,9 +15,8 @@ Selfassesment_name = 'SA'
 Selfassesment_Account_Submission_path = 'SAS'
 Selfassesment_Account_Submission_name = 'SAS'
 
-SelfassesmentTracker_path = 'SATrc'
-SelfassesmentTracker_name = 'SATrc'
-
+Selfassesment_Tracker_path = 'SATrc'
+Selfassesment_Tracker_name = 'SATrc'
 
 
 Limited_path = 'LTD'
@@ -28,3 +27,167 @@ Limited_Account_Submission_name = 'LAS'
 
 LimitedTracker_path = 'LTDTrc'
 LimitedTracker_name = 'LTDTrc'
+
+
+class Dict_Duck_type:
+  def get_dict(self):
+    attars = {}
+    for attar in dir(self):
+      if not callable(attar) and not (attar.startswith('__') or attar.endswith('__')):
+        attars[attar] = getattr(self, attar)
+    return attars
+  
+  def __getitem__(self, key:str) -> str:
+    return getattr(self, key, '')
+  
+  def __setitem__(self, key:str, value) -> None:
+    setattr(self, key, value)
+
+
+# Only use in urls.py to set url routes
+class URL_PATHS(Dict_Duck_type):
+  # Selfassesment
+  Selfassesment_home_url = f'{Selfassesment_path}/{home_suffix}/'
+  Selfassesment_create_url = f'{Selfassesment_path}/{create_suffix}/'
+  Selfassesment_update_url = f'{Selfassesment_path}/{update_suffix}/<int:client_id>/'
+  Selfassesment_delete_url = f'{Selfassesment_path}/{delete_suffix}/<int:client_id>/'
+  Selfassesment_search_url = f'{Selfassesment_path}/{search_suffix}/<str:search_text>/' # fetch only
+  Selfassesment_viewall_url = f'{Selfassesment_path}/{viewall_suffix}/' # fetch only
+
+  # Selfassesment Account Submission
+  Selfassesment_Account_Submission_home_url = f'{Selfassesment_Account_Submission_path}/{home_suffix}/'
+  Selfassesment_Account_Submission_create_url = f'{Selfassesment_Account_Submission_path}/{create_suffix}/'
+  Selfassesment_Account_Submission_update_url = f'{Selfassesment_Account_Submission_path}/{update_suffix}/<int:submission_id>/'
+  Selfassesment_Account_Submission_delete_url = f'{Selfassesment_Account_Submission_path}/{delete_suffix}/<int:submission_id>/'
+  Selfassesment_Account_Submission_search_url = f'{Selfassesment_Account_Submission_path}/{search_suffix}/<str:search_text>/' # fetch only
+  Selfassesment_Account_Submission_viewall_url = f'{Selfassesment_Account_Submission_path}/{viewall_suffix}/' # fetch only
+  add_all_Selfassesment_to_Selfassesment_Account_Submission_url = f'add_all/{Selfassesment_path}/to/{Selfassesment_Account_Submission_path}/'
+
+  # Tracker
+  Selfassesment_Tracker_home_url = f'{Selfassesment_Tracker_path}/{home_suffix}/'
+  Selfassesment_Tracker_create_url = f'{Selfassesment_Tracker_path}/{create_suffix}/'
+  Selfassesment_Tracker_update_url = f'{Selfassesment_Tracker_path}/{update_suffix}/<int:tracker_id>/'
+  Selfassesment_Tracker_delete_url = f'{Selfassesment_Tracker_path}/{delete_suffix}/<int:tracker_id>/'
+  Selfassesment_Tracker_search_url = f'{Selfassesment_Tracker_path}/{search_suffix}/<str:search_text>/' # fetch only
+  Selfassesment_Tracker_viewall_url = f'{Selfassesment_Tracker_path}/{viewall_suffix}/' # fetch only
+
+
+# Only use in urls.py to set url names
+class URL_NAMES(Dict_Duck_type):
+  # Selfassesment
+  Selfassesment_home_name = f'{Selfassesment_name}_{home_suffix}'
+  Selfassesment_create_name = f'{Selfassesment_name}_{create_suffix}'
+  Selfassesment_update_name = f'{Selfassesment_name}_{update_suffix}'
+  Selfassesment_delete_name = f'{Selfassesment_name}_{delete_suffix}'
+  Selfassesment_search_name = f'{Selfassesment_name}_{search_suffix}' # fetch only
+  Selfassesment_viewall_name = f'{Selfassesment_name}_{viewall_suffix}' # fetch only
+
+  # Selfassesment Account Submission
+  Selfassesment_Account_Submission_home_name = f'{Selfassesment_Account_Submission_name}_{home_suffix}'
+  Selfassesment_Account_Submission_create_name = f'{Selfassesment_Account_Submission_name}_{create_suffix}'
+  Selfassesment_Account_Submission_update_name = f'{Selfassesment_Account_Submission_name}_{update_suffix}'
+  Selfassesment_Account_Submission_delete_name = f'{Selfassesment_Account_Submission_name}_{delete_suffix}'
+  Selfassesment_Account_Submission_search_name = f'{Selfassesment_Account_Submission_name}_{search_suffix}' # fetch only
+  Selfassesment_Account_Submission_viewall_name = f'{Selfassesment_Account_Submission_name}_{viewall_suffix}' # fetch only
+  add_all_Selfassesment_to_Selfassesment_Account_Submission_name = f'add_all_{Selfassesment_name}_to_{Selfassesment_Account_Submission_name}'
+
+  # Tracker
+  Selfassesment_Tracker_home_name = f'{Selfassesment_Tracker_name}_{home_suffix}'
+  Selfassesment_Tracker_create_name = f'{Selfassesment_Tracker_name}_{create_suffix}'
+  Selfassesment_Tracker_update_name = f'{Selfassesment_Tracker_name}_{update_suffix}'
+  Selfassesment_Tracker_delete_name = f'{Selfassesment_Tracker_name}_{delete_suffix}'
+  Selfassesment_Tracker_search_name = f'{Selfassesment_Tracker_name}_{search_suffix}' # fetch only
+  Selfassesment_Tracker_viewall_name = f'{Selfassesment_Tracker_name}_{viewall_suffix}' # fetch only
+
+
+# Should be passed in templates for JavaScript to fetch() api calls
+class Full_URL_PATHS_WITHOUT_ARGUMENTS(Dict_Duck_type):
+  # Selfassesment
+  Selfassesment_home_url = f'/{APPLICATION_NAME}/{Selfassesment_path}/{home_suffix}/'
+  Selfassesment_create_url = f'/{APPLICATION_NAME}/{Selfassesment_path}/{create_suffix}/'
+  Selfassesment_update_url = f'/{APPLICATION_NAME}/{Selfassesment_path}/{update_suffix}/'
+  Selfassesment_delete_url = f'/{APPLICATION_NAME}/{Selfassesment_path}/{delete_suffix}/'
+  Selfassesment_search_url = f'/{APPLICATION_NAME}/{Selfassesment_path}/{search_suffix}/' # fetch only
+  Selfassesment_viewall_url = f'/{APPLICATION_NAME}/{Selfassesment_path}/{viewall_suffix}/' # fetch only
+
+  # Selfassesment Account Submission
+  Selfassesment_Account_Submission_home_url = f'/{APPLICATION_NAME}/{Selfassesment_Account_Submission_path}/{home_suffix}/'
+  Selfassesment_Account_Submission_create_url = f'/{APPLICATION_NAME}/{Selfassesment_Account_Submission_path}/{create_suffix}/'
+  Selfassesment_Account_Submission_update_url = f'/{APPLICATION_NAME}/{Selfassesment_Account_Submission_path}/{update_suffix}/'
+  Selfassesment_Account_Submission_delete_url = f'/{APPLICATION_NAME}/{Selfassesment_Account_Submission_path}/{delete_suffix}/'
+  Selfassesment_Account_Submission_search_url = f'/{APPLICATION_NAME}/{Selfassesment_Account_Submission_path}/{search_suffix}/' # fetch only
+  Selfassesment_Account_Submission_viewall_url = f'/{APPLICATION_NAME}/{Selfassesment_Account_Submission_path}/{viewall_suffix}/' # fetch only
+  add_all_Selfassesment_to_Selfassesment_Account_Submission_url = f'add_all/{Selfassesment_path}/to/{Selfassesment_Account_Submission_path}/'
+
+  # Tracker
+  Selfassesment_Tracker_home_url = f'/{APPLICATION_NAME}/{Selfassesment_Tracker_path}/{home_suffix}/'
+  Selfassesment_Tracker_create_url = f'/{APPLICATION_NAME}/{Selfassesment_Tracker_path}/{create_suffix}/'
+  Selfassesment_Tracker_update_url = f'/{APPLICATION_NAME}/{Selfassesment_Tracker_path}/{update_suffix}/'
+  Selfassesment_Tracker_delete_url = f'/{APPLICATION_NAME}/{Selfassesment_Tracker_path}/{delete_suffix}/'
+  Selfassesment_Tracker_search_url = f'/{APPLICATION_NAME}/{Selfassesment_Tracker_path}/{search_suffix}/' # fetch only
+  Selfassesment_Tracker_viewall_url = f'/{APPLICATION_NAME}/{Selfassesment_Tracker_path}/{viewall_suffix}/' # fetch only
+
+
+# Should be used in templates to reffer to links
+class URL_NAMES_PREFIXED_WITH_APP_NAME(Dict_Duck_type):
+  # Selfassesment
+  Selfassesment_home_name = f'{APPLICATION_NAME}:{Selfassesment_name}_{home_suffix}'
+  Selfassesment_create_name = f'{APPLICATION_NAME}:{Selfassesment_name}_{create_suffix}'
+  Selfassesment_update_name = f'{APPLICATION_NAME}:{Selfassesment_name}_{update_suffix}'
+  Selfassesment_delete_name = f'{APPLICATION_NAME}:{Selfassesment_name}_{delete_suffix}'
+  Selfassesment_search_name = f'{APPLICATION_NAME}:{Selfassesment_name}_{search_suffix}' # fetch only
+  Selfassesment_viewall_name = f'{APPLICATION_NAME}:{Selfassesment_name}_{viewall_suffix}' # fetch only
+
+  # Selfassesment Account Submission
+  Selfassesment_Account_Submission_home_name = f'{APPLICATION_NAME}:{Selfassesment_Account_Submission_name}_{home_suffix}'
+  Selfassesment_Account_Submission_create_name = f'{APPLICATION_NAME}:{Selfassesment_Account_Submission_name}_{create_suffix}'
+  Selfassesment_Account_Submission_update_name = f'{APPLICATION_NAME}:{Selfassesment_Account_Submission_name}_{update_suffix}'
+  Selfassesment_Account_Submission_delete_name = f'{APPLICATION_NAME}:{Selfassesment_Account_Submission_name}_{delete_suffix}'
+  Selfassesment_Account_Submission_search_name = f'{APPLICATION_NAME}:{Selfassesment_Account_Submission_name}_{search_suffix}' # fetch only
+  Selfassesment_Account_Submission_viewall_name = f'{APPLICATION_NAME}:{Selfassesment_Account_Submission_name}_{viewall_suffix}' # fetch only
+  add_all_Selfassesment_to_Selfassesment_Account_Submission_name = f'{APPLICATION_NAME}:add_all_{Selfassesment_name}_to_{Selfassesment_Account_Submission_name}'
+
+  # Tracker
+  Selfassesment_Tracker_home_name = f'{APPLICATION_NAME}:{Selfassesment_Tracker_name}_{home_suffix}'
+  Selfassesment_Tracker_create_name = f'{APPLICATION_NAME}:{Selfassesment_Tracker_name}_{create_suffix}'
+  Selfassesment_Tracker_update_name = f'{APPLICATION_NAME}:{Selfassesment_Tracker_name}_{update_suffix}'
+  Selfassesment_Tracker_delete_name = f'{APPLICATION_NAME}:{Selfassesment_Tracker_name}_{delete_suffix}'
+  Selfassesment_Tracker_search_name = f'{APPLICATION_NAME}:{Selfassesment_Tracker_name}_{search_suffix}' # fetch only
+  Selfassesment_Tracker_viewall_name = f'{APPLICATION_NAME}:{Selfassesment_Tracker_name}_{viewall_suffix}' # fetch only
+
+
+def gen_urls(
+    prefixes=['Selfassesment', 'Selfassesment_Account_Submission', 'Selfassesment_Tracker'],
+    paths=['home', 'create', 'viewall'],
+    paths_w_args=['update', 'delete'],
+    id_param='<int:client_id>',
+    search='search',
+    open='{',
+    close='}'):
+  for prefix in prefixes:
+    for path in paths:
+      print(f"{prefix}_{path}_url = f'{open}{prefix}_path{close}/{open}{path}_suffix{close}/'")
+    for path in paths_w_args:
+      print(f"{prefix}_{path}_url = f'{open}{prefix}_path{close}/{open}{path}_suffix{close}/{id_param}/'")
+    print(f"{prefix}_search_url = f'{open}{prefix}_path{close}/{open}{search}_suffix{close}/<str:search_text>/'")
+    print('')
+
+def gen_names(
+    prefixes=['Selfassesment', 'Selfassesment_Account_Submission', 'Selfassesment_Tracker'],
+    paths=['home', 'create', 'update', 'delete', 'search', 'viewall'],
+    open='{',
+    close='}'):
+  for prefix in prefixes:
+    for path in paths:
+      print(f"{prefix}_{path}_name = f'{open}{prefix}_name{close}_{open}{path}_suffix{close}'")
+    print('')
+
+def gen_names(
+    prefixes=['Selfassesment', 'Selfassesment_Account_Submission', 'Selfassesment_Tracker'],
+    paths=['home', 'create', 'update', 'delete', 'search', 'viewall'],
+    open='{',
+    close='}'):
+  for prefix in prefixes:
+    for path in paths:
+      print(f"{prefix}_{path}_name = f'{open}APPLICATION_NAME{close}:{open}{prefix}_name{close}_{open}{path}_suffix{close}'")
+    print('')
--- companies/urls.py
+++ companies/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.urls.conf import re_path
 
 # selfassesment view functions
-from .views import add_all_limited_to_limited_account_submission_w_submission_year, home_selfassesment, create_selfassesment, update_selfassesment, \
+from .views import URL_path_names, add_all_limited_to_limited_account_submission_w_submission_year, home_selfassesment, create_selfassesment, update_selfassesment, \
     delete_selfassesment, search_selfassesment, all_selfassesment
 
 # selfassesment account submission view function
@@ -22,157 +22,169 @@ from .views import home_limited_account_submission, create_limited_account_submi
     add_all_limited_to_limited_account_submission_w_submission_year
 from .views import home_limited_tracker, create_limited_tracker, update_limited_tracker, delete_limited_tracker, \
     search_limited_tracker, all_limited_tracker
-    
+
+from .views import create_test
+
+from .url_variables import APPLICATION_NAME, URL_PATHS, URL_NAMES
 from .url_variables import *
 
-app_name = application_name
+
+app_name = APPLICATION_NAME
+application_name = app_name
 
 urlpatterns = [
-    path(route='', 
+    path(route='',
         view=home_selfassesment,
         name='home'),
-    
+
+    path(route='test/',
+        view=create_test,
+        name='test'),
+
     # =============================================================================================================
     # =============================================================================================================
     # Selfassesment
-    path(route=f'{Selfassesment_path}/{home_suffix}/',
-        view=home_selfassesment,
-        name=f'{Selfassesment_name}_{home_suffix}'),
-    
-    path(route=f'{Selfassesment_path}/{create_suffix}/',
-        view=create_selfassesment,
-        name=f'{Selfassesment_name}_{create_suffix}'),
+    path(route = URL_PATHS.Selfassesment_home_url,
+        view = home_selfassesment,
+        name = URL_NAMES.Selfassesment_home_name),
 
-    path(route=f'{Selfassesment_path}/{update_suffix}/<int:client_id>/', 
-        view=update_selfassesment, 
-        name=f'{Selfassesment_name}_{update_suffix}'),
+    path(route = URL_PATHS.Selfassesment_create_url,
+        view = create_selfassesment,
+        name = URL_NAMES.Selfassesment_create_name),
 
-    path(route=f'{Selfassesment_path}/{delete_suffix}/<int:client_id>/', 
-        view=delete_selfassesment, 
-        name=f'{Selfassesment_name}_{delete_suffix}'),
+    path(route = URL_PATHS.Selfassesment_update_url,
+        view = update_selfassesment,
+        name = URL_NAMES.Selfassesment_update_name),
 
-    path(route=f'{Selfassesment_path}/{search_suffix}/<str:search_text>/', 
-        view=search_selfassesment, 
-        name=f'{Selfassesment_name}_{search_suffix}'), # fetch only
+    path(route = URL_PATHS.Selfassesment_delete_url,
+        view = delete_selfassesment,
+        name= URL_NAMES.Selfassesment_delete_name),
 
-    path(route=f'{Selfassesment_path}/{viewall_suffix}/', 
-        view=all_selfassesment, 
-        name=f'{Selfassesment_name}_{viewall_suffix}'), # fetch only
+    path(route = URL_PATHS.Selfassesment_search_url,
+        view = search_selfassesment,
+        name = URL_NAMES.Selfassesment_search_name), # fetch only
+
+    path(route = URL_PATHS.Selfassesment_viewall_url,
+        view = all_selfassesment,
+        name = URL_NAMES.Selfassesment_viewall_name), # fetch only
 
 
     # SelfassesmentAccountSubmission
-    path(route=f'{Selfassesment_Account_Submission_path}/{home_suffix}/', 
-        view=home_selfassesment_account_submission, 
-        name=f'{Selfassesment_Account_Submission_name}_{home_suffix}'),
+    path(route = URL_PATHS.Selfassesment_Account_Submission_home_url,
+        view = home_selfassesment_account_submission,
+        name = URL_NAMES.Selfassesment_Account_Submission_home_name),
 
-    path(route=f'{Selfassesment_Account_Submission_path}/{create_suffix}/', 
-        view=create_selfassesment_account_submission, 
-        name=f'{Selfassesment_Account_Submission_name}_{create_suffix}'),
+    path(route = URL_PATHS.Selfassesment_Account_Submission_create_url,
+        view = create_selfassesment_account_submission,
+        name = URL_NAMES.Selfassesment_Account_Submission_create_name),
 
-    path(route=f'{Selfassesment_Account_Submission_path}/{update_suffix}/<int:submission_id>/', 
-        view=update_selfassesment_account_submission, 
-        name=f'{Selfassesment_Account_Submission_name}_{update_suffix}'),
+    path(route = URL_PATHS.Selfassesment_Account_Submission_update_url,
+        view = update_selfassesment_account_submission,
+        name = URL_NAMES.Selfassesment_Account_Submission_update_name),
 
-    path(route=f'{Selfassesment_Account_Submission_path}/{delete_suffix}/<int:submission_id>/', 
-        view=delete_selfassesment_account_submission, 
-        name=f'{Selfassesment_Account_Submission_name}_{delete_suffix}'),
-    
-    path(route=f'{Selfassesment_Account_Submission_path}/{search_suffix}/<str:search_text>/', 
-        view=search_selfassesment_account_submission, 
-        name=f'{Selfassesment_Account_Submission_name}_{search_suffix}'),
-    
-    path(route=f'{Selfassesment_Account_Submission_path}/{viewall_suffix}/', 
-        view=all_selfassesment_account_submission, 
-        name=f'{Selfassesment_Account_Submission_name}_{viewall_suffix}'),
+    path(route = URL_PATHS.Selfassesment_Account_Submission_delete_url,
+        view = delete_selfassesment_account_submission,
+        name = URL_NAMES.Selfassesment_Account_Submission_delete_name),
+
+    path(route = URL_PATHS.Selfassesment_Account_Submission_search_url,
+        view = search_selfassesment_account_submission,
+        name = URL_NAMES.Selfassesment_Account_Submission_search_name),
+
+    path(route = URL_PATHS.Selfassesment_Account_Submission_viewall_url,
+        view = all_selfassesment_account_submission,
+        name = URL_NAMES.Selfassesment_Account_Submission_viewall_name),
 
     # Add all Selfassesment to SelfassesmentAccountSubmission
-    path(route=f'add_all/{Selfassesment_path}/to/{Selfassesment_Account_Submission_path}/', 
-        view=add_all_selfassesment_to_selfassesment_account_submission_w_submission_year, 
-        name=f'add_all_{Selfassesment_name}_to_{Selfassesment_Account_Submission_name}'),
-    
+    path(route = URL_PATHS.add_all_Selfassesment_to_Selfassesment_Account_Submission_url,
+        view = add_all_selfassesment_to_selfassesment_account_submission_w_submission_year,
+        name = URL_NAMES.add_all_Selfassesment_to_Selfassesment_Account_Submission_name),
+
+
     # =============================================================================================================
     # SelfassesmentTracker
-    path(route=f'{SelfassesmentTracker_path}/{home_suffix}/',
-        view=home_selfassesment_tracker,
-        name=f'{SelfassesmentTracker_name}_{home_suffix}'),
+    path(route = URL_PATHS.Selfassesment_Tracker_home_url,
+        view = home_selfassesment_tracker,
+        name = URL_NAMES.Selfassesment_Tracker_home_name),
 
-    path(route=f'{SelfassesmentTracker_path}/{create_suffix}/',
-        view=create_selfassesment_tracker,
-        name=f'{SelfassesmentTracker_name}_{create_suffix}'),
+    path(route = URL_PATHS.Selfassesment_Tracker_create_url,
+        view = create_selfassesment_tracker,
+        name = URL_NAMES.Selfassesment_Tracker_create_name),
 
-    path(route=f'{SelfassesmentTracker_path}/{update_suffix}/<int:tracker_id>/', 
-        view=update_selfassesment_tracker, 
-        name=f'{SelfassesmentTracker_name}_{update_suffix}'),
+    path(route = URL_PATHS.Selfassesment_Tracker_update_url,
+        view = update_selfassesment_tracker,
+        name = URL_NAMES.Selfassesment_Tracker_update_name),
 
-    path(route=f'{SelfassesmentTracker_path}/{delete_suffix}/<int:tracker_id>/', 
-        view=delete_selfassesment_tracker, 
-        name=f'{SelfassesmentTracker_name}_{delete_suffix}'),
+    path(route = URL_PATHS.Selfassesment_Tracker_delete_url,
+        view = delete_selfassesment_tracker,
+        name = URL_NAMES.Selfassesment_Tracker_delete_name),
 
-    path(route=f'{SelfassesmentTracker_path}/{search_suffix}/<str:search_text>/', 
-        view=search_selfassesment_tracker, 
-        name=f'{SelfassesmentTracker_name}_{search_suffix}'),
+    path(route = URL_PATHS.Selfassesment_Tracker_search_url,
+        view = search_selfassesment_tracker,
+        name = URL_NAMES.Selfassesment_Tracker_search_name),
 
-    path(route=f'{SelfassesmentTracker_path}/{viewall_suffix}/', 
-        view=all_selfassesment_tracker, 
-        name=f'{SelfassesmentTracker_name}_{viewall_suffix}'),
+    path(route = URL_PATHS.Selfassesment_Tracker_viewall_url,
+        view = all_selfassesment_tracker,
+        name = URL_NAMES.Selfassesment_Tracker_viewall_name),
+
+
     # =============================================================================================================
     # =============================================================================================================
     # Limited
     path(route=f'{Limited_path}/{home_suffix}/',
         view=home_limited,
         name=f'{Limited_name}_{home_suffix}'),
-    
+
     path(route=f'{Limited_path}/{create_suffix}/',
         view=create_limited,
         name=f'{Limited_name}_{create_suffix}'),
 
-    path(route=f'{Limited_path}/{update_suffix}/<int:client_id>/', 
-        view=update_limited, 
+    path(route=f'{Limited_path}/{update_suffix}/<int:client_id>/',
+        view=update_limited,
         name=f'{Limited_name}_{update_suffix}'),
 
-    path(route=f'{Limited_path}/{delete_suffix}/<int:client_id>/', 
-        view=delete_limited, 
+    path(route=f'{Limited_path}/{delete_suffix}/<int:client_id>/',
+        view=delete_limited,
         name=f'{Limited_name}_{delete_suffix}'),
 
-    path(route=f'{Limited_path}/{search_suffix}/<str:search_text>/', 
-        view=search_limited, 
+    path(route=f'{Limited_path}/{search_suffix}/<str:search_text>/',
+        view=search_limited,
         name=f'{Limited_name}_{search_suffix}'), # fetch only
 
-    path(route=f'{Limited_path}/{viewall_suffix}/', 
-        view=all_limited, 
+    path(route=f'{Limited_path}/{viewall_suffix}/',
+        view=all_limited,
         name=f'{Limited_name}_{viewall_suffix}'), # fetch only
 
 
     # LimitedAccountSubmission
-    path(route=f'{Limited_Account_Submission_path}/{home_suffix}/', 
-        view=home_limited_account_submission, 
+    path(route=f'{Limited_Account_Submission_path}/{home_suffix}/',
+        view=home_limited_account_submission,
         name=f'{Limited_Account_Submission_name}_{home_suffix}'),
 
-    path(route=f'{Limited_Account_Submission_path}/{create_suffix}/', 
-        view=create_limited_account_submission, 
+    path(route=f'{Limited_Account_Submission_path}/{create_suffix}/',
+        view=create_limited_account_submission,
         name=f'{Limited_Account_Submission_name}_{create_suffix}'),
 
-    path(route=f'{Limited_Account_Submission_path}/{update_suffix}/<int:submission_id>/', 
-        view=update_limited_account_submission, 
+    path(route=f'{Limited_Account_Submission_path}/{update_suffix}/<int:submission_id>/',
+        view=update_limited_account_submission,
         name=f'{Limited_Account_Submission_name}_{update_suffix}'),
 
-    path(route=f'{Limited_Account_Submission_path}/{delete_suffix}/<int:submission_id>/', 
-        view=delete_limited_account_submission, 
+    path(route=f'{Limited_Account_Submission_path}/{delete_suffix}/<int:submission_id>/',
+        view=delete_limited_account_submission,
         name=f'{Limited_Account_Submission_name}_{delete_suffix}'),
-    
-    path(route=f'{Limited_Account_Submission_path}/{search_suffix}/<str:search_text>/', 
-        view=search_limited_account_submission, 
+
+    path(route=f'{Limited_Account_Submission_path}/{search_suffix}/<str:search_text>/',
+        view=search_limited_account_submission,
         name=f'{Limited_Account_Submission_name}_{search_suffix}'),
-    
-    path(route=f'{Limited_Account_Submission_path}/{viewall_suffix}/', 
-        view=all_limited_account_submission, 
+
+    path(route=f'{Limited_Account_Submission_path}/{viewall_suffix}/',
+        view=all_limited_account_submission,
         name=f'{Limited_Account_Submission_name}_{viewall_suffix}'),
 
     # Add all Limited to LimitedAccountSubmission
-    path(route=f'add_all/{Limited_path}/to/{Limited_Account_Submission_path}/', 
-        view=add_all_limited_to_limited_account_submission_w_submission_year, 
+    path(route=f'add_all/{Limited_path}/to/{Limited_Account_Submission_path}/',
+        view=add_all_limited_to_limited_account_submission_w_submission_year,
         name=f'add_all_{Limited_name}_to_{Limited_Account_Submission_name}'),
-        
+
     # =============================================================================================================
     # =============================================================================================================
     # LimitedTracker
@@ -184,19 +196,19 @@ urlpatterns = [
         view=create_limited_tracker,
         name=f'{LimitedTracker_name}_{create_suffix}'),
 
-    path(route=f'{LimitedTracker_path}/{update_suffix}/<int:tracker_id>/', 
-        view=update_limited_tracker, 
+    path(route=f'{LimitedTracker_path}/{update_suffix}/<int:tracker_id>/',
+        view=update_limited_tracker,
         name=f'{LimitedTracker_name}_{update_suffix}'),
 
-    path(route=f'{LimitedTracker_path}/{delete_suffix}/<int:tracker_id>/', 
-        view=delete_limited_tracker, 
+    path(route=f'{LimitedTracker_path}/{delete_suffix}/<int:tracker_id>/',
+        view=delete_limited_tracker,
         name=f'{LimitedTracker_name}_{delete_suffix}'),
 
-    path(route=f'{LimitedTracker_path}/{search_suffix}/<str:search_text>/', 
-        view=search_limited_tracker, 
+    path(route=f'{LimitedTracker_path}/{search_suffix}/<str:search_text>/',
+        view=search_limited_tracker,
         name=f'{LimitedTracker_name}_{search_suffix}'),
 
-    path(route=f'{LimitedTracker_path}/{viewall_suffix}/', 
-        view=all_limited_tracker, 
+    path(route=f'{LimitedTracker_path}/{viewall_suffix}/',
+        view=all_limited_tracker,
         name=f'{LimitedTracker_name}_{viewall_suffix}'),
 ]

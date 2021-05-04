0113f3ab91bdcf5a1af1e886dacbc613a9fa400f
Iftakhar Husan <iftakharn19@gmail.com>
2021-05-04 06:47:53 +0600

    add filter tags and update dir filter tag

--- companies/templatetags/companies_tags.py
+++ companies/templatetags/companies_tags.py
@@ -1,9 +1,11 @@
 from django import template
 from django.utils.safestring import mark_safe
 from pprint import pp
+import json
 
 
 register = template.Library()
+
 
 @register.filter(name='addstr')
 def addstr(arg1, arg2):
@@ -11,6 +13,27 @@ def addstr(arg1, arg2):
     concatinated = str(arg1) + str(arg2)
     return mark_safe(concatinated)
 
-@register.filter(name='dir')
-def addstr(arg1):
-    return dir(arg1)
+@register.filter('dir')
+def get_dir(obj):
+    attrs = {}
+    for attr in dir(obj):
+        attrs[attr] = str(getattr(obj, attr, None))
+    response = json.dumps(attrs, indent=4)
+    return response
+
+
+@register.filter(name='get_attr')
+def get_nested_attr(obj, attr, default=None, attr_split_on='.'):
+    """Filter tag to get python object's attributes.
+    Supportes nested attributes separated by '.'.
+    If attribute doesn't exists returns None as default.
+    """
+    attrs = attr.split(attr_split_on)
+    value = obj
+    for attr in attrs:
+        value = getattr(value, attr, default)
+    return value
+
+@register.filter(name='get_field')
+def get_field(form, field_name):
+    return form[field_name]

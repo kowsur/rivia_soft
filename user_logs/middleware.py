800751cabb84ad57c17d33436714a9cb1f753d06
Iftakhar Husan <iftakharn19@gmail.com>
2021-12-08 15:57:21 +0600

    change middleware to decorator

--- user_logs/middleware.py
+++ user_logs/middleware.py
@@ -2,12 +2,23 @@ from .models import ActiveUser, UserLoginHistory
 from django.contrib.auth import get_user_model
 
 
-class UserLastSeenLoggerMiddleware():
-    def __init__(self, get_response) -> None:
-        self.get_response = get_response
+# class UserLastSeenLoggerMiddleware():
+#     def __init__(self, get_response) -> None:
+#         self.get_response = get_response
     
-    def __call__(self, request):
-        response = self.get_response(request)
+#     def __call__(self, request):
+#         response = self.get_response(request)
+#         if isinstance(request.user, get_user_model()):
+#             user = request.user
+#             active_user_log = ActiveUser.objects.get(user=user)
+#             active_user_log.save()
+#             user_login_history = active_user_log.user_login_history
+#             user_login_history.save()
+#         return response
+
+def UserLastSeenLoggerMiddleware(get_response):
+    def middleware(request):
+        response = get_response(request)
         if isinstance(request.user, get_user_model()):
             user = request.user
             active_user_log = ActiveUser.objects.get(user=user)
@@ -15,3 +26,4 @@ class UserLastSeenLoggerMiddleware():
             user_login_history = active_user_log.user_login_history
             user_login_history.save()
         return response
+    return middleware

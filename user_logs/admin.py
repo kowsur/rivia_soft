586895cb6e44aa46e72a673223a2b7cd6c945e28
Iftakhar Husan <iftakharn19@gmail.com>
2021-12-08 10:36:50 +0600

    add models to admin

--- user_logs/admin.py
+++ user_logs/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import ActiveUser, FailedLoginAttempts, UserLoginHistory
 
 # Register your models here.
+admin.site.register(ActiveUser)
+admin.site.register(UserLoginHistory)
+admin.site.register(FailedLoginAttempts)
--- user_logs/signals.py
+++ user_logs/signals.py
@@ -7,23 +7,23 @@ import json
 
 @receiver(user_logged_in)
 def log_user_login(sender, request, user, **kwargs):
-    print(request.META, kwargs)
+    print("login", request.META, kwargs)
     logged_in_user_history = UserLoginHistory(user=user, ip_address=request.META.get("REMOTE_ADDR"), device_user_agent=request.META.get("HTTP_USER_AGENT"))
     logged_in_user_history.save()
     active_user = ActiveUser(user_login_history=logged_in_user_history, user=user, session=request.session)
     active_user.save()
-
  
  
 @receiver(user_login_failed)
 def log_user_login_failed(sender, credentials, request, **kwargs):
-    print(kwargs, credentials)
+    print("failed login", kwargs, credentials)
     failed_attempt = FailedLoginAttempts(ip_address=request.META.get("REMOTE_ADDR"), device_user_agent=request.META.get("HTTP_USER_AGENT"), credentials=json.dumps(credentials, indent=4))
     failed_attempt.save()
  
  
 @receiver(user_logged_out)
 def log_user_logout(sender, request, user, **kwargs):
+    print("logout", request, user)
     for active_user in ActiveUser.objects.filter(user=user):
         sessions = Session.objects.filter(session_key=ActiveUser.session.session_key)
         for session in sessions:

ad30e0ca166073e7dc3f6fa3149d07499e8c0747
Iftakhar Husan <iftakharn19@gmail.com>
2021-12-08 10:10:34 +0600

    Add user_logs

---
+++ user_logs/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.contrib.sessions.models import Session
+from django.conf.global_settings import AUTH_USER_MODEL
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+class UserLoginHistory(models.Model):
+    class Meta:
+        verbose_name = _("User Login History")
+        verbose_name_plural = _("User Login Histories")
+    
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
+    ip_address = models.IPAddressField(verbose_name="Public ip address of device", )
+    device_user_agent = models.TextField(verbose_name="User Agent")
+    logged_in_at = models.DateTimeField(auto_now_add=True)
+    last_seen_at = models.DateTimeField(auto_now=True)
+
+
+class FailedLoginAttempts(models.Model):
+    class Meta:
+        verbose_name = _("Failed User Login History")
+        verbose_name_plural = _("Failed User Login Histories")
+    
+    ip_address = models.IPAddressField(verbose_name="Public ip address of device")
+    credentials = models.TextField()
+    device_user_agent = models.TextField(verbose_name="User Agent")
+    tried_to_logged_in_at = models.DateTimeField(auto_now_add=True)
+
+
+class ActiveUser(models.Model):
+    class Meta:
+        verbose_name = _("Active User")
+        verbose_name_plural = _("Active Users")
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
+    session = models.ForeignKey(Session, on_delete=models.CASCADE)
+    user_login_history = models.ForeignKey(UserLoginHistory, on_delete=models.CASCADE)
+    last_seen_at = models.DateTimeField(auto_now=True)
---
+++ user_logs/signals.py
@@ -0,0 +1,31 @@
+from django.contrib.sessions.models import Session
+from django.dispatch import receiver
+from django.contrib.auth.signals import user_logged_in, user_logged_out, user_login_failed
+from .models import ActiveUser, UserLoginHistory, FailedLoginAttempts
+import json
+
+
+@receiver(user_logged_in)
+def log_user_login(sender, request, user, **kwargs):
+    print(request.META, kwargs)
+    logged_in_user_history = UserLoginHistory(user=user, ip_address=request.META.get("REMOTE_ADDR"), device_user_agent=request.META.get("HTTP_USER_AGENT"))
+    logged_in_user_history.save()
+    active_user = ActiveUser(user_login_history=logged_in_user_history, user=user, session=request.session)
+    active_user.save()
+
+ 
+ 
+@receiver(user_login_failed)
+def log_user_login_failed(sender, credentials, request, **kwargs):
+    print(kwargs, credentials)
+    failed_attempt = FailedLoginAttempts(ip_address=request.META.get("REMOTE_ADDR"), device_user_agent=request.META.get("HTTP_USER_AGENT"), credentials=json.dumps(credentials, indent=4))
+    failed_attempt.save()
+ 
+ 
+@receiver(user_logged_out)
+def log_user_logout(sender, request, user, **kwargs):
+    for active_user in ActiveUser.objects.filter(user=user):
+        sessions = Session.objects.filter(session_key=ActiveUser.session.session_key)
+        for session in sessions:
+            session.delete()
+        active_user.delete()

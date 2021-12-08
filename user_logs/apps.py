abac6d31ffac67a9017264776adb7347cddd0ac8
Iftakhar Husan <iftakharn19@gmail.com>
2021-12-08 10:45:48 +0600

    On app ready register signals

--- user_logs/apps.py
+++ user_logs/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class UserLogsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'user_logs'
+    
+    def ready(self) -> None:
+        import user_logs.signals
+        return super().ready()

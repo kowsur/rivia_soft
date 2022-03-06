5e6ae8c49af87cec0796bb58ee77b092fc1e87d2
Iftakhar Husan <iftakharn19@gmail.com>
2022-03-07 00:18:07 +0600

    fix accounts index page

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 
 from companies.views import URLS
+from companies.decorators import allowed_for_staff, allowed_for_superuser
 
 
+@allowed_for_staff()
 def index(request):
     context = {**URLS}
     return render(request, "accounts/index.html", context)

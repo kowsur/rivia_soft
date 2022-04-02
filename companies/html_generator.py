d29db17dc4bc1c28fc353f289fa72e31437d78e8
Iftakhar Husan <iftakharn19@gmail.com>
2022-04-03 02:55:39 +0600

    Add new fk_field

--- companies/html_generator.py
+++ companies/html_generator.py
@@ -52,6 +52,7 @@ def generate_template_tag_for_model(
       'prepared_by': { 'details_url_without_argument': user_details_url_without_argument, 'repr-format': HTML_Generator.CustomUser_repr_format },
       'assigned_to': { 'details_url_without_argument': user_details_url_without_argument, 'repr-format': HTML_Generator.CustomUser_repr_format },
       'client_id': { 'details_url_without_argument': Full_URL_PATHS_WITHOUT_ARGUMENTS.Selfassesment_details_url, 'repr-format': HTML_Generator.Selfassesment_client_id_repr_format, 'href-url':Full_URL_PATHS_WITHOUT_ARGUMENTS.Selfassesment_update_url },
+      'selfassesment': { 'details_url_without_argument': Full_URL_PATHS_WITHOUT_ARGUMENTS.Selfassesment_details_url, 'repr-format': HTML_Generator.Selfassesment_client_id_repr_format, 'href-url':Full_URL_PATHS_WITHOUT_ARGUMENTS.Selfassesment_update_url },
       'incomplete_tasks': { 'details_url_without_argument': '/companies/SATrc/search/?client_id=', 'repr-format': r'{length}'},
       'tax_year': { 'details_url_without_argument': Full_URL_PATHS_WITHOUT_ARGUMENTS.Selfassesment_Account_Submission_Tax_Year_details_url, 'repr-format': HTML_Generator.Selfassemsent_tax_year_repr_format },
       }

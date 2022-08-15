98b72a7daed1a042123c4768b45e3e067ec4466c
Iftakhar Husan <iftakharn19@gmail.com>
2022-08-15 21:54:17 +0600

    Update template generator to handle custom fields

--- companies/html_generator.py
+++ companies/html_generator.py
@@ -12,14 +12,14 @@ def get_field_names_from_model(django_model:models.Model):
     field_names.append(field.name)
   return field_names
 
-def get_header_name_from_field_name(django_model, field_name):
+def get_header_name_from_field_name(django_model, field_name:str):
   try:
     return django_model._meta.get_field(field_name).verbose_name
   except:
     try:
       return django_model._meta.get_field(field_name).name
     except:
-      return 'Incomplete Tasks'
+      return ' '.join(map(lambda word: word.capitalize(), field_name.split('_')))
 
 def is_includeable(field, include_fields=[], exclude_fields=[], keep_include_fields=True, show_others=False):
   # skip if exclude_fields contains the field and keep_include_fields is False
@@ -53,8 +53,9 @@ def generate_template_tag_for_model(
       'assigned_to': { 'details_url_without_argument': user_details_url_without_argument, 'repr-format': HTML_Generator.CustomUser_repr_format },
       'client_id': { 'details_url_without_argument': Full_URL_PATHS_WITHOUT_ARGUMENTS.Selfassesment_details_url, 'repr-format': HTML_Generator.Selfassesment_client_id_repr_format, 'href-url':Full_URL_PATHS_WITHOUT_ARGUMENTS.Selfassesment_update_url },
       'selfassesment': { 'details_url_without_argument': Full_URL_PATHS_WITHOUT_ARGUMENTS.Selfassesment_details_url, 'repr-format': HTML_Generator.Selfassesment_client_id_repr_format, 'href-url':Full_URL_PATHS_WITHOUT_ARGUMENTS.Selfassesment_update_url },
-      'incomplete_tasks': { 'details_url_without_argument': '/companies/SATrc/search/?client_id=', 'repr-format': r'{length}'},
       'tax_year': { 'details_url_without_argument': Full_URL_PATHS_WITHOUT_ARGUMENTS.Selfassesment_Account_Submission_Tax_Year_details_url, 'repr-format': HTML_Generator.Selfassemsent_tax_year_repr_format },
+      'incomplete_tasks': { 'details_url_without_argument': '/companies/SATrc/search/?client_id=', 'repr-format': r'{length}', 'data-field': 'pk', 'href-url':'/companies/SATrc/home/?client_id='},
+      'company_registration_number': { 'details_url_without_argument': '/companies/LTD/details/', 'repr-format': r'{company_reg_number}', 'data-field': 'fields.client_id'},
       }
   ):
   
@@ -110,22 +111,24 @@ def generate_template_tag_for_model(
         inner_template_tr += f'<td class="data-cell" id="pk"></td>\n'
       continue
     if field in fk_fields:
-      if field=='incomplete_tasks':
-        inner_template_tr+=f"""
-        <td class="data-cell" id="{field}"
-          data-field="pk" data-url="{fk_fields[field]['details_url_without_argument']}" data-repr-format="{fk_fields[field]['repr-format']}"></td>\n"""
-      else:
-        data_attrs = f'''
-        data-field="fields.{field}" 
+      data_attrs = f"""
         data-url="{fk_fields[field]['details_url_without_argument']}" 
         data-repr-format="{fk_fields[field]['repr-format']}" 
+        """
+      if fk_fields[field].get('data-field'):
+        data_attrs += f'''
+          data-field="{fk_fields[field].get('data-field')}"
         '''
-        if fk_fields[field].get('href-url'):
-          data_attrs += f'''data-href-url= "{fk_fields[field]['href-url']}"'''
+      else:
+        data_attrs += f'''
+          data-field="fields.{field}"
+        '''
+      if fk_fields[field].get('href-url'):
+        data_attrs += f'''data-href-url="{fk_fields[field]['href-url']}"'''
 
-        inner_template_tr+=f"""
-        <td class="data-cell" id="{field}"
-          {data_attrs}></td>\n"""
+      inner_template_tr+=f"""
+      <td class="data-cell" id="{field}"
+        {data_attrs}></td>\n"""
     else:
       inner_template_tr += f'<td class="data-cell" id="{field}"></td>\n'
   

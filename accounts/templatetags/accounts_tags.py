175690539830974005d7658f8a0d7d22e1ba9f71
Iftakhar Husan <iftakharn19@gmail.com>
2023-06-26 02:11:55 +0300

    Round all the numbers

--- accounts/templatetags/accounts_tags.py
+++ accounts/templatetags/accounts_tags.py
@@ -26,3 +26,26 @@ def add(number1, number2):
         return float(number1) + float(number2)
     except Exception:
         return "Error"
+
+
+@register.filter(name="natural_round")
+def natural_round(number):
+    try:
+        number = float(number)
+
+        # Cascade rounding the floating digits from right to left
+        floating_digits = [int(digit) for digit in str(number).split(".")[-1]]
+        idx = len(floating_digits)-1
+        while idx > 0:
+            if floating_digits[idx]>=5:
+                floating_digits[idx-1] += 1
+            idx -= 1
+        
+        # Cascade round to the integer part
+        number = int(number)
+        if floating_digits[0] >= 5:
+            number += 1
+        
+        return number
+    except Exception:
+        return "Error"

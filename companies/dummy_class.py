3e8c5123481c9820bd6545159d041e4bdb7708ac
Iftakhar Husan <iftakharn19@gmail.com>
2025-01-01 16:24:37 +0200

    Update dummy classes

--- companies/dummy_class.py
+++ companies/dummy_class.py
@@ -14,30 +14,59 @@ class SearchableModelField:
 class SelfassesmentCreationForm:
         def __init__(self, *args, **kwargs) -> None:
                 pass
+
 class SelfassesmentChangeForm:
         def __init__(self, *args, **kwargs) -> None:
                 pass
+
 class SelfassesmentDeleteForm:
         def __init__(self, *args, **kwargs) -> None:
                 pass
+
+class SelfemploymentIncomeAndExpensesDataCollectionAuthFormForClients:
+        def __init__(self, *args, **kwargs) -> None:
+                pass
+
+class SelfemploymentIncomeAndExpensesDataCollectionCreationFormForClients:
+        def __init__(self, *args, **kwargs) -> None:
+                pass
+
+class SelfemploymentIncomeAndExpensesDataCollectionCreationForm:
+        def __init__(self, *args, **kwargs) -> None:
+                pass
+
+class SelfemploymentIncomeAndExpensesDataCollectionUpdateForm:
+        def __init__(self, *args, **kwargs) -> None:
+                pass
+
+class SelfemploymentIncomeAndExpensesDataCollectionDeleteForm:
+        def __init__(self, *args, **kwargs) -> None:
+                pass
+
 class SelfassesmentAccountSubmissionCreationForm:
         def __init__(self, *args, **kwargs) -> None:
                 pass
+
 class SelfassesmentAccountSubmissionChangeForm:
         def __init__(self, *args, **kwargs) -> None:
                 pass
+
 class SelfassesmentAccountSubmissionDeleteForm:
         def __init__(self, *args, **kwargs) -> None:
                 pass
+
 class Add_All_Selfassesment_to_SelfassesmentAccountSubmission_Form:
         def __init__(self, *args, **kwargs) -> None:
                 pass
+
 class SelfassesmentTrackerCreationForm:
         def __init__(self, *args, **kwargs) -> None:
                 pass
+
 class SelfassesmentTrackerChangeForm:
         def __init__(self, *args, **kwargs) -> None:
                 pass
+
 class SelfassesmentTrackerDeleteForm:
         def __init__(self, *args, **kwargs) -> None:
                 pass
@@ -45,27 +74,68 @@ class SelfassesmentTrackerDeleteForm:
 class LimitedCreationForm:
         def __init__(self, *args, **kwargs) -> None:
                 pass
+
 class LimitedChangeForm:
         def __init__(self, *args, **kwargs) -> None:
                 pass
+
 class LimitedDeleteForm:
         def __init__(self, *args, **kwargs) -> None:
                 pass
+
+class LimitedOnboardingForm:
+        def __init__(self, *args, **kwargs) -> None:
+                pass
+
 class LimitedTrackerCreationForm:
         def __init__(self, *args, **kwargs) -> None:
                 pass
+
 class LimitedTrackerChangeForm:
         def __init__(self, *args, **kwargs) -> None:
                 pass
+
 class LimitedTrackerDeleteForm:
         def __init__(self, *args, **kwargs) -> None:
                 pass
+
+class MergedTrackerCreateionForm:
+        def __init__(self, *args, **kwargs) -> None:
+                pass
+
 class LimitedSubmissionDeadlineTrackerCreationForm:
         def __init__(self, *args, **kwargs) -> None:
                 pass
+
 class LimitedSubmissionDeadlineTrackerChangeForm:
         def __init__(self, *args, **kwargs) -> None:
                 pass
+
 class LimitedSubmissionDeadlineTrackerDeleteForm:
         def __init__(self, *args, **kwargs) -> None:
                 pass
+
+class LimitedVATTrackerCreationForm:
+        def __init__(self, *args, **kwargs) -> None:
+                pass
+
+class LimitedVATTrackerChangeForm:
+        def __init__(self, *args, **kwargs) -> None:
+                pass
+
+class LimitedVATTrackerDeleteForm:
+        def __init__(self, *args, **kwargs) -> None:
+                pass
+
+class LimitedConfirmationStatementTrackerCreationForm:
+        def __init__(self, *args, **kwargs) -> None:
+                pass
+
+class LimitedConfirmationStatementTrackerChangeForm:
+        def __init__(self, *args, **kwargs) -> None:
+                pass
+
+class LimitedConfirmationStatementTrackerDeleteForm:
+        def __init__(self, *args, **kwargs) -> None:
+                pass
+

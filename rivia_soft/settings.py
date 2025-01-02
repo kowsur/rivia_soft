d373e518fd097ab893ad77af39d081734c4a8697
Iftakhar Husan <iftakharn19@gmail.com>
2025-01-02 03:24:25 +0200

    Disable ssl settings

--- rivia_soft/settings.py
+++ rivia_soft/settings.py
@@ -200,10 +200,10 @@ REST_FRAMEWORK = {
 if not DEBUG:
     # python manage.py check --deploy
     # ?: (security.W004) You have not set a value for the SECURE_HSTS_SECONDS setting. If your entire site is served only over SSL, you may want to consider setting a value and enabling HTTP Strict Transport Security. Be sure to read the documentation first; enabling HSTS carelessly can cause serious, irreversible problems.
-    # SECURE_HSTS_SECONDS = 10 # 1800
+    # SECURE_HSTS_SECONDS = 10 # 1800 # nginx will handle this
 
     # ?: (security.W008) Your SECURE_SSL_REDIRECT setting is not set to True. Unless your site should be available over both SSL and non-SSL connections, you may want to either set this setting True or configure a load balancer or reverse-proxy server to redirect all connections to HTTPS.       
-    SECURE_SSL_REDIRECT = True
+    # SECURE_SSL_REDIRECT = True # behind nginx so no need for this
 
     # ?: (security.W012) SESSION_COOKIE_SECURE is not set to True. Using a secure-only session cookie makes it more difficult for network traffic sniffers to hijack user sessions.
     SESSION_COOKIE_SECURE = True

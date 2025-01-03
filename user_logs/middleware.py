ec87d0ffc754b07c9e5a6c83e720e55608525a0e
Iftakhar Husan <iftakharn19@gmail.com>
2025-01-03 10:41:24 +0200

    Try 2 fix error in middleware

--- user_logs/middleware.py
+++ user_logs/middleware.py
@@ -1,20 +1,53 @@
 from django.contrib.auth import get_user_model
-from django.db.models import Exception
+from django.contrib.sessions.models import Session
 from .models import ActiveUser, UserLoginHistory
 
 
 def UserLastSeenLoggerMiddleware(get_response):
     def middleware(request):
         response = get_response(request)
-        if isinstance(request.user, get_user_model()):
-            user = request.user
-            try:
-                active_user_log = ActiveUser.objects.get(user=user)
-                active_user_log.save()
-                user_login_history = active_user_log.user_login_history
-                user_login_history.save()
-            except Exception:
-                pass
+        if not isinstance(request.user, get_user_model()):
+            return response
+        
+        user = request.user
+        if ActiveUser.objects.filter(user=user).exists():
+            active_user_log = ActiveUser.objects.get(user=user)
+            active_user_log.save()
+            user_login_history = active_user_log.user_login_history
+            user_login_history.save()
+            return response
+        
+        active_devices = ActiveUser.objects.filter(user=user)
+        if len(active_devices)>0:
+            for active_device in active_devices:
+                # updateing login_history
+                login_history = active_device.user_login_history
+                login_history.logged_out_at = now()
+                login_history.save()
+
+                session = active_device.session
+                session.delete()
+                
+                active_device.delete()
+
+        session = Session.objects.get(pk=request.session.session_key)
+        
+        logged_in_user_history = UserLoginHistory(
+            user=user,
+            ip_address=request.META.get("HTTP_X_FORWARDED_FOR"),
+            device_user_agent=request.META.get("HTTP_USER_AGENT"))
+        logged_in_user_history.save()
+        
+        active_user = ActiveUser(
+            user_login_history=logged_in_user_history,
+            user=user,
+            session=session)
+        active_user.save()
+
+        active_user_log = ActiveUser.objects.get(user=user)
+        active_user_log.save()
+        user_login_history = active_user_log.user_login_history
+        user_login_history.save()
         return response
     return middleware
 
--- user_logs/signals.py
+++ user_logs/signals.py
@@ -6,32 +6,32 @@ from .models import ActiveUser, UserLoginHistory, FailedLoginAttempts
 import json
 
 
-@receiver(user_logged_in)
-def log_user_login(sender, request, user, **kwargs):
-    active_devices = ActiveUser.objects.filter(user=user)
-    if len(active_devices)>0:
-        for active_device in active_devices:
-            # updateing login_history
-            login_history = active_device.user_login_history
-            login_history.logged_out_at = now()
-            login_history.save()
+# @receiver(user_logged_in)
+# def log_user_login(sender, request, user, **kwargs):
+#     active_devices = ActiveUser.objects.filter(user=user)
+#     if len(active_devices)>0:
+#         for active_device in active_devices:
+#             # updateing login_history
+#             login_history = active_device.user_login_history
+#             login_history.logged_out_at = now()
+#             login_history.save()
 
-            session = active_device.session
-            session.delete()
+#             session = active_device.session
+#             session.delete()
             
-            active_device.delete()
+#             active_device.delete()
 
-    session = Session.objects.get(pk=request.session.session_key)
-    logged_in_user_history = UserLoginHistory(
-        user=user,
-        ip_address=request.META.get("HTTP_X_FORWARDED_FOR"),
-        device_user_agent=request.META.get("HTTP_USER_AGENT"))
-    logged_in_user_history.save()
-    active_user = ActiveUser(
-        user_login_history=logged_in_user_history,
-        user=user,
-        session=session)
-    active_user.save()
+#     session = Session.objects.get(pk=request.session.session_key)
+#     logged_in_user_history = UserLoginHistory(
+#         user=user,
+#         ip_address=request.META.get("HTTP_X_FORWARDED_FOR"),
+#         device_user_agent=request.META.get("HTTP_USER_AGENT"))
+#     logged_in_user_history.save()
+#     active_user = ActiveUser(
+#         user_login_history=logged_in_user_history,
+#         user=user,
+#         session=session)
+#     active_user.save()
 
 
 @receiver(user_login_failed)

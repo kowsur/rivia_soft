fcdf5f3583e3f6d8818194e54119dd575a713b85
Iftakhar Husan <iftakharn19@gmail.com>
2025-01-04 18:56:13 +0200

    Update date time formats

--- user_logs/admin.py
+++ user_logs/admin.py
@@ -1,21 +1,49 @@
 from django.contrib import admin
 from django.urls import reverse
 from django.utils.safestring import mark_safe
+from django.utils.timezone import localtime
+from django.utils.formats import date_format
 from django.contrib.auth import get_user_model
 from django.contrib.admin import SimpleListFilter
 from django.contrib.sessions.models import Session
 from .models import ActiveUser, FailedLoginAttempts, UserLoginHistory
 
 
-class UserLoginHistoryAdmin(admin.ModelAdmin):
+class OverrideAdminDatetimeFormats:
+    def format_datetime(self, obj, field_name):
+        # Generic method to format datetime fields
+        field_value = getattr(obj, field_name, None)
+        if field_value:
+            local_time = localtime(field_value)
+            return date_format(local_time, use_l10n=True, format="y/n/j\u00a0H:i:s")
+        return "-"
+
+    def add_formatted_datetime_fields(self):
+        # Dynamically add formatted fields ending with "at"
+        fields = [field.name for field in self.model._meta.fields if field.name.endswith("at")]
+        for field in fields:
+            # Add dynamic method to admin
+            method_name = f"_formatted__{field}"
+            setattr(self, method_name, lambda obj, field=field: self.format_datetime(obj, field))
+                # Configure display name and sorting
+            getattr(self, method_name).short_description = field.replace("_", " ").capitalize()
+            getattr(self, method_name).admin_order_field = field
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.add_formatted_datetime_fields()
+
+
+class UserLoginHistoryAdmin(OverrideAdminDatetimeFormats, admin.ModelAdmin):
     model = UserLoginHistory
-    list_display = ("user", "ip_address", "device_user_agent", "logged_in_at", "last_seen_at", "logged_out_at", )
+    list_display = ("user", "ip_address", "device_user_agent", "_formatted__logged_in_at", "_formatted__logged_out_at", )
     list_filter = ("logged_in_at", "last_seen_at", "logged_out_at", "user", "ip_address", )
     search_fields = ("user__email", "ip_address", "device_user_agent", )
     ordering = ("-logged_in_at", "-last_seen_at")
+
     
 
-class ActiveUserAdmin(admin.ModelAdmin):
+class ActiveUserAdmin(OverrideAdminDatetimeFormats, admin.ModelAdmin):
     model = ActiveUser
     list_display = ("user", "session", "last_seen_at", "user_login_history", )
     list_filter = ("last_seen_at", "user", )
@@ -25,7 +53,7 @@ class ActiveUserAdmin(admin.ModelAdmin):
 
 
 
-class FailedLoginAttemptsAdmin(admin.ModelAdmin):
+class FailedLoginAttemptsAdmin(OverrideAdminDatetimeFormats, admin.ModelAdmin):
     model = FailedLoginAttempts
     list_display = ("tried_to_log_in_at", "ip_address", "credentials", "device_user_agent", )
     list_filter = ("tried_to_log_in_at", "ip_address",)
